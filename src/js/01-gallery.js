ba05351abbd17c3c7cfad48e2db04a49bf0060e9
OlyaNegrych <olyanegrych@gmail.com>
2022-08-03 19:05:52 +0300

    installed libraries

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,4 @@
 import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-//.... правки ментор.............
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from "./gallery-items";
