39cc1d8df16439fa4c5fbe6df77fdbd020e50952
OlyaNegrych <olyanegrych@gmail.com>
2022-08-03 14:39:37 +0300

    mentor adit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+//.... правки ментор.............
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from "./gallery-items";
